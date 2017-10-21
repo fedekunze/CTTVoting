dc98358580e00201cfe5da32c63d9f71351bfe19
kevin <kevinc_2019@berkeley.edu>
2017-10-21 14:21:11 -0700

    first version of token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,13 +7,38 @@ If you deploy this, you won't have anything useful.
 Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
 .*/
 
-pragma solidity ^0.4.5;
+pragma solidity ^0.4.15;
 
 import "./TokenInterface.sol";
 
 contract Token is TokenInterface {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
+		mapping (address => uint256) balances;
+  	mapping (address => bool) lockedAccounts;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+
+  	address votingMechContract;
+  	address superVotingContract:
+
+  	modifier notLocked(address account) {
+    		require(!lockedAccounts[account]);
+      	_;
+    }
+  	modifier isVotingMech() {
+    		require(msg.sender == votingMechContract);
+      	_;
+    }
+  	modifier isSuperVoting() {
+    		require(msg.sender == superVotingContract);
+      	_;
+    }
+
+  	function Token(address _superVotingContract) {
+    		superVotingContract = _superVotingContract;
+    }
+
+    function transfer(address _to, uint256 _value) notLocked(msg.sender) returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
@@ -26,7 +51,7 @@ contract Token is TokenInterface {
         } else { return false; }
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) notLocked(_from) returns (bool success) {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
@@ -49,10 +74,22 @@ contract Token is TokenInterface {
     }
 
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
+      	return allowed[_owner][_spender];
     }
 
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
+  	/* sets new voting mechanism */
+  	function newVotingMechanism(address _votingMechContract) isSuperVoting() public {
+    		votingMechContract = _votingMechContract;
+    }
+  	/* locks account for address */
+  	function lockAccount(address account) isVotingMech() public {
+    		lockedAccounts[account] = true;
+    }
+  	/* unlocks account for address, call after everyone has revealed ans has been charged */
+  	function unlockAccount(address account, uint256 newBalance) isVotingMech() public {
+      	/* update total supply after force update */
+      	totalSupply = totalSupply + newBalance - balances[account];
+      	balances[account] = newBalance;
+    		lockedAccounts[account] = false;
+    }
 }
