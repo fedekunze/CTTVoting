d7292857f47e33480e8e76158f5da6c6d3c77ab1
kevin <kevinc_2019@berkeley.edu>
2017-10-21 15:41:18 -0700

    SuperVoting

---
+++ contracts/SuperVoting.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.15;
+
+/* This contract is to decide which voting mechanism we are using */
+import "./Token.sol";
+
+contract SuperVoting {
+
+	address votingMechContract;
+  address admin;
+  Token tok;
+
+  modifier isAdmin() {
+  		require(msg.sender == admin);
+    	_;
+  }
+
+  function SuperVoting(address _admin) public {
+  		admin = _admin;
+  }
+
+  function setTokenAddress(address _tokenAddr) isAdmin() public {
+      tok = Token(_tokenaddr);
+  }
+
+  function setVotingMech(address _votingMechContract) isAdmin() public {
+    	/* setting voting mechanism */
+  		votingMechContract = _votingMechContract;
+
+    	/* call token to update its votingmech contract */
+    	tok.newVotingMechanism(votingMechContract);
+  }
+}
