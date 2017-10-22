f0f7b37d4a718b13aaa51743427d1f3e182c417e
kevin <kevinc_2019@berkeley.edu>
2017-10-21 21:56:47 -0700

    added test functions to token and supervoting

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,32 +9,32 @@ import "./TokenInterface.sol";
 
 contract Token is TokenInterface {
 
-		mapping (address => uint256) balances;
+    mapping (address => uint256) balances;
   	mapping (address => bool) lockedAccounts;
     mapping (address => mapping (address => uint256)) allowed;
     uint256 public totalSupply;
 
   	address votingMechContract;
-  	address superVotingContract:
+  	address superVotingContract;
 
   	modifier notLocked(address account) {
-    		require(!lockedAccounts[account]);
+        require(!lockedAccounts[account]);
       	_;
     }
   	modifier isVotingMech() {
-    		require(msg.sender == votingMechContract);
+        require(msg.sender == votingMechContract);
       	_;
     }
   	modifier isSuperVoting() {
-    		require(msg.sender == superVotingContract);
+        require(msg.sender == superVotingContract);
       	_;
     }
 
-  	function Token(address _superVotingContract) {
-    		superVotingContract = _superVotingContract;
+  	function Token(address _superVotingContract) public{
+        superVotingContract = _superVotingContract;
     }
 
-    function transfer(address _to, uint256 _value) notLocked(msg.sender) returns (bool success) {
+    function transfer(address _to, uint256 _value) notLocked(msg.sender) public returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
@@ -47,7 +47,7 @@ contract Token is TokenInterface {
         } else { return false; }
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) notLocked(_from) returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) notLocked(_from) public returns (bool success) {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
@@ -59,35 +59,32 @@ contract Token is TokenInterface {
         } else { return false; }
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) constant public returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) constant public returns (uint256 remaining) {
       	return allowed[_owner][_spender];
     }
 
   	/* sets new voting mechanism */
   	function newVotingMechanism(address _votingMechContract) isSuperVoting() public {
-    		votingMechContract = _votingMechContract;
+        votingMechContract = _votingMechContract;
     }
   	/* locks account for address */
   	function lockAccount(address account) isVotingMech() public {
-    		lockedAccounts[account] = true;
+        lockedAccounts[account] = true;
     }
   	/* unlocks account for address, call after everyone has revealed ans has been charged */
   	function unlockAccount(address account, uint256 newBalance) isVotingMech() public {
-      	/* update total supply after force update */
-//       	totalSupply = totalSupply + newBalance - balances[account];
-//       	balances[account] = newBalance;
       	forceUpdate(account, newBalance);
-    		lockedAccounts[account] = false;
+    	lockedAccounts[account] = false;
     }
   	/* unlocks account for address, call after everyone has revealed ans has been charged */
   	function forceUpdate(address account, uint256 newBalance) isVotingMech() public {
@@ -95,4 +92,8 @@ contract Token is TokenInterface {
       	totalSupply = totalSupply + newBalance - balances[account];
       	balances[account] = newBalance;
     }
+
+    // function getVotingMech() public returns(address) {
+    //     return votingMechContract;
+    // }
 }
