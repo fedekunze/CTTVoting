54ffe4ab1eb589fbd1da6ed0b89bb252231eb97c
kevin <kevinc_2019@berkeley.edu>
2017-10-22 03:42:07 -0700

    changed deploy files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,20 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var SuperVoting = artifacts.require("./SuperVoting.sol");
+var Token = artifacts.require("./Token.sol");
+var VotingMechanism = artifacts.require("./VotingMechanism.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = function(deployer, networks, accounts) {
+  deployer.deploy(SuperVoting, accounts[0]).then(function() {
+    deployer.deploy(Token, SuperVoting.address).then(function() {
+      deployer.deploy(VotingMechanism, accounts[0], Token.address).then(function() {
+        var sv;
+        deployer.then(function() {
+          return SuperVoting.deployed();
+        }).then(function(instance) {
+          sv = instance;
+          sv.setTokenAddress(Token.address);
+          sv.setVotingMech(VotingMechanism.address);
+        });
+      });
+    });
+  });
 };
